9627bdd0bd5c6ea7fe9002208845afc5b52e85c3
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-06-28 18:10:13 -0300

    CLASSES in C++: [ https://youtu.be/2BP8NhxjrO0 ]

--- BMOAulasCpp10ClassesInCpp/main.cpp
+++ BMOAulasCpp10ClassesInCpp/main.cpp
@@ -8,8 +8,29 @@
 
 #include <iostream>
 
+#define Log(x) std::cout << x << std::endl
+
+class Jogador {
+public:
+    int x,y;
+    int velocidade;
+    void Mover(int xa, int ya) {
+        x += xa * velocidade;
+        y += ya * velocidade;
+    }
+}; //Sempre nessessario ';'
+
+/*void Mover(Jogador& jogador, int xa, int ya) {              //Isso é uma função
+    jogador.x += xa * jogador.velocidade;
+    jogador.y += ya * jogador.velocidade;
+}*/
+
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    Jogador jogador;
+    jogador.Mover(1, -1);
+    jogador.x = 5;
+    
     return 0;
 }
