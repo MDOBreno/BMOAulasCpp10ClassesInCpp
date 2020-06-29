aa70d8856044d89abc905e21d3bb900e02c226c9
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-06-28 23:47:53 -0300

    - Aula concluida.

--- BMOAulasCpp10ClassesInCpp/main.cpp
+++ BMOAulasCpp10ClassesInCpp/main.cpp
@@ -10,8 +10,11 @@
 
 #define Log(x) std::cout << x << std::endl
 
-class Jogador {
-public:
+//#define struct class    //Serve paar manter a compatibilidade de Struct's da linguagem C, para o C++.
+
+//A diferença entre Struct e Class é que a primeira por padrao atrib/metod são publicos,classes o contrario.
+
+struct JogadorStruct {
     int x,y;
     int velocidade;
     void Mover(int xa, int ya) {
@@ -20,17 +23,39 @@ public:
     }
 }; //Sempre nessessario ';'
 
-/*void Mover(Jogador& jogador, int xa, int ya) {              //Isso é uma função
+class JogadorClasse {
+public:
+    int x,y;
+    int velocidade;
+    void Mover(int xa, int ya) {                                //Isso é um método.
+        x += xa * velocidade;
+        y += ya * velocidade;
+    }
+}; //Sempre nessessario ';'
+
+/*void Mover(Jogador& jogador, int xa, int ya) {                //Isso é uma função.
     jogador.x += xa * jogador.velocidade;
     jogador.y += ya * jogador.velocidade;
 }*/
 
+struct vec2 {
+    float x,y;
+    void Add(const vec2& outro){
+        x += outro.x;
+        y += outro.y;
+    }
+};
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     
-    Jogador jogador;
-    jogador.Mover(1, -1);
-    jogador.x = 5;
+    JogadorClasse jogadorClasse;
+    jogadorClasse.Mover(1, -1);
+    jogadorClasse.x = 5;
+    
+    JogadorStruct jogadorStruct;
+    jogadorStruct.Mover(1, -1);
+    jogadorStruct.x = 5;
     
     return 0;
 }
